4a1fe87d641c128178831c59716934574c4a8ab0
Jaime Fabián Valenzuela Soto <jaimefvs@gmail.com>
2018-06-06 22:09:32 -0400

    Test 3.1

--- run_module.py
+++ run_module.py
@@ -18,50 +18,46 @@ def getDocument(id):
     return cursor
 
 def run(doc_id):
-    try:
-        print('STEP 1')
-        #First
-        document = getDocument(doc_id)
-        data = []
-        clf = utils.loadData('/var/www/html/scrapper/PCGA/models/topmodel95.41.pickle')
+    print('STEP 1')
+    #First
+    document = getDocument(doc_id)
+    data = []
+    clf = utils.loadData('/var/www/html/scrapper/PCGA/models/topmodel95.41.pickle')
 
-        for account in document[0]['data']:
-            aux = []
-            aux.append(account['name'])
-            aux.append(account['assets'])
-            aux.append(account['liabilities'])
-            aux.append(account['lost'])
-            aux.append(account['gain'])
-            
-            df = pd.DataFrame(aux)
-            df.columns = ['text', 'val5', 'val6', 'val7', 'val8']
-            df = utils.cleanData(df)
-
-            nlp = spacy.load('es')
-
-            doc = nlp(df['text'].strip().lower())
-            df['text'] = doc.vector_norm
-
-            print(df)
-            
-            aux = []
-            for row in df.iterrows():
-                aux.append(row[1]['text'])
-                aux.append(utils.numberToBinary(row[1]['val5']))
-                aux.append(utils.numberToBinary(row[1]['val6']))
-                aux.append(utils.numberToBinary(row[1]['val7']))
-                aux.append(utils.numberToBinary(row[1]['val8']))
-
-            predicted = clf.predict([aux])
-
-            account['group'] = predicted[0]
+    for account in document[0]['data']:
+        aux = []
+        aux.append(account['name'])
+        aux.append(account['assets'])
+        aux.append(account['liabilities'])
+        aux.append(account['lost'])
+        aux.append(account['gain'])
         
-        print(document[0]['data'])
+        df = pd.DataFrame(aux)
+        df.columns = ['text', 'val5', 'val6', 'val7', 'val8']
+        df = utils.cleanData(df)
 
-        print('--1 CHECK--')
+        nlp = spacy.load('es')
 
-    except Exception as e:
-        print(str(e))
+        doc = nlp(df['text'].strip().lower())
+        df['text'] = doc.vector_norm
+
+        print(df)
+        
+        aux = []
+        for row in df.iterrows():
+            aux.append(row[1]['text'])
+            aux.append(utils.numberToBinary(row[1]['val5']))
+            aux.append(utils.numberToBinary(row[1]['val6']))
+            aux.append(utils.numberToBinary(row[1]['val7']))
+            aux.append(utils.numberToBinary(row[1]['val8']))
+
+        predicted = clf.predict([aux])
+
+        account['group'] = predicted[0]
+    
+    print(document[0]['data'])
+
+    print('--1 CHECK--')
 
 if __name__ == '__main__':
     print('--HOLI :) --')
